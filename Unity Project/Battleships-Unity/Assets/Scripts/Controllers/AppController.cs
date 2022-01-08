875f76bbdadc27d804e7f7d079f9cc011d99c6db
Jack Brewer <jack@hungrydevstudios.com>
2022-01-08 15:12:07 +0000

    chore: Finished project set up

--- Unity Project/Battleships-Unity/Assets/Scripts/Controllers/AppController.cs
+++ Unity Project/Battleships-Unity/Assets/Scripts/Controllers/AppController.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using FriedSynapse.Quickit;
 
-public class AppController : MonoBehaviour
+public class AppController : Singleton<AppController>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
